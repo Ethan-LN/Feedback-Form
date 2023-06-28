fc29b52af05693d010a1fe60ba65c47ce8100822
Ethan <et.l.nicholas@gmail.com>
2023-06-28 22:36:02 +1000

    setup the componets: HappinessPicker and feedbackForm

---
+++ src/components/feedbackForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import HappinessPicker from './happinessPicker'
+import '../styles/feedbackForm.css'
+
+const FeedbackForm = () => {
+  return (
+    <div className='form'>
+        <HappinessPicker />
+        <p>hello</p>
+    </div>
+  )
+}
+
+export default FeedbackForm
---
+++ src/components/happinessPicker.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/happinessPicker.css'
+
+const HappinessPicker = () => {
+  return (
+    <div>
+        <p>HappinessPicker</p>
+    </div>
+  )
+}
+
+export default HappinessPicker
