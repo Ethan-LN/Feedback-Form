55db8e391b5c7e1634f60dfa6c70b8bb49d3b10a
Ethan <et.l.nicholas@gmail.com>
2023-06-29 10:25:25 +1000

    add total scoresfor food, service and value for money

--- src/components/happinessPicker.js
+++ src/components/happinessPicker.js
@@ -1,12 +1,19 @@
-import React from 'react'
-import '../styles/happinessPicker.css'
+import React from "react";
+import "../styles/happinessPicker.css";
 
-const HappinessPicker = () => {
+const HappinessPicker = (props) => {
+  const emojiScore = ["😩", "🙁", "😐", "🙂", "😁"];
+
   return (
-    <div>
-        <p>HappinessPicker</p>
+    <div className="btns__group">
+      <p>{props.category}</p>
+      <button onClick={() => props.changeValue(0)}>{emojiScore[0]}</button>
+      <button onClick={() => props.changeValue(1)}>{emojiScore[1]}</button>
+      <button onClick={() => props.changeValue(2)}>{emojiScore[2]}</button>
+      <button onClick={() => props.changeValue(3)}>{emojiScore[3]}</button>
+      <button onClick={() => props.changeValue(4)}>{emojiScore[4]}</button>
     </div>
-  )
-}
+  );
+};
 
-export default HappinessPicker
+export default HappinessPicker;
